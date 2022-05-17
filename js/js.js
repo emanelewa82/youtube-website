46d47278e680fe324fdb65528a895e19d4262614
emanelewa82 <elewaeman@gmail.com>
2022-05-18 00:01:16 +0200

    youtube website

---
+++ js/js.js
@@ -0,0 +1,9 @@
+/*navbar side*/
+function openBar() {
+   document.getElementById("mySideBar").style.width = "350px";
+    document.getElementById("overlay").style.display = "block";
+}
+function closeBar() {
+   document.getElementById("mySideBar").style.width = "0";
+    document.getElementById("overlay").style.display = "none";
+}
